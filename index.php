85a4f9b854cea7ea290ece7bf714329b902eb14a
Naoki Shimizu <38304893+naoki-shimizu@users.noreply.github.com>
2018-10-23 11:20:13 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-  echo "<h1>Good-by world! /h1>\n"; 
+  echo "<h1>Good-by world!</h1>\n"; 
   echo "hostname: " . gethostname() . "<BR>\n";
   echo "IP-Addr : " . $_SERVER['SERVER_ADDR'] . "\n";
 ;?>
